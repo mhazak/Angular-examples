723b0865af4ff28abd5502e469d8e7ef3d7d375b
hazak.marek <hazak.marek@gmail.com>
2024-02-03 22:15:07 +0100

    takeUntilDestroy init

---
+++ src/app/components/subscribe/base-example/takeUntilDestroy.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-takeUntilDestroy',
+  template: `abc`,
+})
+export class TakeUntilDestroyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
